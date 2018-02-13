10edd2223fd88213f17be40189658dbdc9006427
thomas.litton <thomas.litton@centurylink.com>
2018-02-13 13:41:02 -0600

    Fixing backup script

--- roles/os_tools/files/backup.sh
+++ roles/os_tools/files/backup.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /var/log/backup
 
-rsync -av ~thomas.litton/ --exclude-file=~thomas.litton/backup-exclusions.txt /mnt/backup/home > /var/log/backup/thomaslitton.log
+rsync -av ~thomas.litton/ --exclude-from=~thomas.litton/backup-exclusions.txt /mnt/backup/home > /var/log/backup/thomaslitton.log
 rsync -av /etc/ /mnt/backup/etc > /var/log/backup/etc.log
